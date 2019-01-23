2879d96d9628aa7624b556b01b48b3a519292daa
Rithie <euler.sistemas@gmail.com>
2019-01-23 08:28:47 -0200

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Euler Rithiele Alvarenga on 1/23/19.
+//  Copyright © 2019 Euler Rithiele Alvarenga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
